0f661a581456abc2ea03cd5624db2f075a17e328
Erik Slagter <erik.slagter@strawberries.nl>
2017-08-25 09:41:47 +0200

    Created example using svg-inline-loader with Nuxt.js

---
+++ pages/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        svg-icons
+      </h1>
+      <h2 class="subtitle">
+        Nuxt.js project
+      </h2>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+      </div>
+
+      <svg role="img" viewBox="0 0 100 100" class="icon shape-twitter">
+        <use xlink:href="#shape-twitter"></use>
+      </svg>
+
+      <svg v-html="require('~/assets/sprite.svg')"></svg>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+  created () {
+    require('~/assets/sprite.svg')
+  }
+}
+</script>
+
+<style>
+.container
+{
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.title
+{
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+.subtitle
+{
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+.links
+{
+  padding-top: 15px;
+}
+</style>
